898fe4951e5211acf9580b7972669b0528e7a2c0
Hannah <hannah_wight@hotmail.com>
2014-03-13 11:27:57 +0000

    lib and spec files created

---
+++ lib/cell.rb
@@ -0,0 +1,19 @@
+class Cell
+
+	attr_accessor :value
+
+	def initialize(neighbours)
+		@neighbours = neighbours
+	end
+
+	def filled_in?
+		#
+	end
+	
+	def possible_candidates
+		#
+	end
+
+	def solve
+	end
+end
---
+++ lib/grid.rb
@@ -0,0 +1,11 @@
+class Grid
+	def initialize
+		@cells = cells
+	end
+	
+	attr_reader :cells
+	
+	def solve
+		#
+	end
+end
